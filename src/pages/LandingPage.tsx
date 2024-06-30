7fbd25e7f90e3b4d21dc5467d7ee15c5a7eb2108
Kayvia Harriott <kayviaharriott@hotmail.com>
2024-06-29 20:54:40 -0500

    Added custom font and env file and sample

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,15 @@
 export const LandingPage = () => {
-    return (
-        <div className="bg-[#01172F] h-dvh flex justify-center items-center">
-           <div className="text-white text-[32px] font-bold"><p>useful shortcuts</p></div>
-        </div>
-    )
-}
+  return (
+    <div className="bg-[#01172F] h-dvh">
+      <div
+        style={{ letterSpacing: "4px" }}
+        className="flex text-[32px] font-[800] py-[56px] flex justify-center"
+      >
+        <p className="text-[#C6EBBE]">useful</p>
+        <p className="text-[#FFFFFF]">shortcuts</p>
+      </div>
+      {/* flex justify-center items-center */}
+      <div className="text-white text-[32px] font-bold"></div>
+    </div>
+  );
+};
