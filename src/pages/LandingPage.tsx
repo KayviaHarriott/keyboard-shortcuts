2dd4b9c3ef6288b44230abf25d3794771be09fdb
Kayvia Harriott <kayviaharriott@hotmail.com>
2024-06-29 20:35:44 -0500

    Added tailwind css

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,7 @@
+export const LandingPage = () => {
+    return (
+        <div className="bg-[#01172F] h-dvh flex justify-center items-center">
+           <div className="text-white text-[32px] font-bold"><p>useful shortcuts</p></div>
+        </div>
+    )
+}
